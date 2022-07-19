67fc8ebd7ce5bc151133d345678a0a47dad448f1
Brian Guzmán <brian.guzman@gmail.com>
2022-07-18 22:31:30 -0400

    primer proyecto con Spring Boot

---
+++ src/main/java/cl/sustantiva/sttdemo/SttdemoApplication.java
@@ -0,0 +1,13 @@
+package cl.sustantiva.sttdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SttdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SttdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/sustantiva/sttdemo/SttdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sustantiva.sttdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SttdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
